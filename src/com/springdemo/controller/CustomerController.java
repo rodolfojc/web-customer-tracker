5752bd25c89b6f16b6e9489db22c4367a142dc9a
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-18 22:13:08 +0000

    Update Customer Controller
    Injecting CustomerDAO

--- src/com/springdemo/controller/CustomerController.java
+++ src/com/springdemo/controller/CustomerController.java
@@ -1,15 +1,33 @@
 package com.springdemo.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.springdemo.dao.CustomerDAO;
+import com.springdemo.entity.Customer;
+
 @Controller
 @RequestMapping("/customer")
 public class CustomerController {
-
+	
+	
+	//INJECT CUSTOMER DAO
+	@Autowired
+	private CustomerDAO customerDAO;
+	
 	@RequestMapping("/list")
-	public String listCustomer(Model themodel) {
+	public String listCustomer(Model theModel) {
+		
+		//GET CUSTOMER
+		List<Customer> theCustomers = customerDAO.getCustomers();
+		
+		//ADD CUSTOMER TO MODEL
+		theModel.addAttribute("customers", theCustomers);
+		
 		
 		return "list-customers";
 	}
