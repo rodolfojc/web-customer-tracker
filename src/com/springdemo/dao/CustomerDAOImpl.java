4f5627e83514e1f1f99518027872cdb24c53c55c
Rodolfo Carvajal <rodolfojcarvajalm@gmail.com>
2019-02-18 22:07:03 +0000

    CustomerDAO
    Added - Interface, with a method to get the customers.

---
+++ src/com/springdemo/dao/CustomerDAOImpl.java
@@ -0,0 +1,38 @@
+package com.springdemo.dao;
+
+import java.util.List;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.query.Query;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.springdemo.entity.Customer;
+
+@Repository
+public class CustomerDAOImpl implements CustomerDAO {
+
+	//INJECT SESSION FACTORY
+	@Autowired
+	private SessionFactory sessionFactory;
+
+	
+	@Override
+	@Transactional
+	public List<Customer> getCustomers() {
+		
+		//GET CURRENT HIBERNATE SESSION
+		Session currentSession = sessionFactory.getCurrentSession();
+		
+		//QUERY
+		Query<Customer> theQuery = currentSession.createQuery("from Customer", Customer.class);
+		
+		//EXECUTE QUERY
+		List<Customer> customers = theQuery.getResultList();
+						
+		return customers;
+	}
+
+}
